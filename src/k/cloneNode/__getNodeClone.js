7e397451d8344d33e43ba2f90e00e07a925706d1
shiauke <shiauke@hotmail.com>
2025-12-22 20:07:56 +0800

    update getNodeClone, k/index.js

--- src/k/cloneNode/__getNodeClone.js
+++ src/k/cloneNode/__getNodeClone.js
@@ -2,12 +2,8 @@ import {cloneNodesRepo} from "./__cloneNodesRepo.js";
 // 開一個map來存prototype node
 
 
-export function getNodeClone(name) {
-  const clone = cloneNodesRepo.get(name);
-  if (!clone) {
-    throw new Error(`Unknown node clone "${name}, please create a clone node first."`);
-  }
-  return clone;
+export function getNodeCloneRepo() {
+  return cloneNodesRepo;
 }
 
 
