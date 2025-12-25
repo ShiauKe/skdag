0dae181d51cb66e9c3f9ae09db995a6617e0c974
shiauke <shiauke@hotmail.com>
2025-12-26 00:22:47 +0800

    update s/index, modify some indents

--- src/s/index.js
+++ src/s/index.js
@@ -1,20 +1,29 @@
 import { k } from "../k/index.js";
-import { topoSortByEdges } from "../k/topoSort/__topoSortByEdges.js";
 import { printGraph, printNeighborhood } from "./observe.js";
 /**
  * Shell API
  * This is the only surface users should touch.
  */
 export const s = Object.freeze({
-  /**
-   * Register a step that can be used in a flow
-   */
-  getProtoTypeRepo(){return k.prototypeNodeRepo},
-  registerPrototypeNode(name, spec) {
-    return k.createNodePrototype(name, spec);
-  },
-  getNodeCloneRepo(){return k.getNodeCloneRepo()},
-  topoSortByEdges(graph){return k.topoSortByEdges(graph)},
-  printGraph(graph){return printGraph(graph)},
-  printNeighborhood(graph, nodeId){return printNeighborhood(graph, nodeId)},
+        getProtoTypeRepo(){
+                return k.prototypeNodeRepo
+        },
+        registerPrototypeNode(name, spec) {
+                return k.createNodePrototype(name, spec);
+        },
+        getNodeCloneRepo(){
+                return k.getNodeCloneRepo()
+        },
+        topoSortByEdges(graph){
+                return k.topoSortByEdges(graph)
+        },
+        printGraph(graph){
+                return printGraph(graph)
+        },
+        printNeighborhood(graph, nodeId){
+                return printNeighborhood(graph, nodeId)
+        },
+        executeGraph(graph){ 
+                return k.executeGraph(graph)
+        }
 });
